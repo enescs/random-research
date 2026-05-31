7ab8027f42e5f8b9278286b53211055b60063a71
enescs <goksueneskerem@gmail.com>
2026-05-31 13:02:28 +0300

    quines: add python quine example

---
+++ quines/quine.py
@@ -0,0 +1,3 @@
+quine='print(f"quine={quine!r}\\n{quine[0:54]}\\n{quine[56:]}")\\n# for a string a, {a!r} means the raw string a)'
+print(f"quine={quine!r}\n{quine[0:54]}\n{quine[56:]}")
+# for a string a, {a!r} means the raw string a)
